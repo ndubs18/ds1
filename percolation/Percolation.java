fe5c46301b1ff0f38bb121f1b408756fd3438439
Nick Winkler <nick.winkler1@gmail.com>
2024-05-07 13:29:34 -0700

    implemented constructor, isOpen, numberOfOpenSites, and validate + flatCoords helper functions

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,24 +1,73 @@
-/* *****************************************************************************
- *  Name:              Alan Turing
- *  Coursera User ID:  123456
- *  Last modified:     1/1/2019
- **************************************************************************** */
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
     boolean[] grid;
-    int n;
+    int N;
+    int nSquared;
+    WeightedQuickUnionUF wqfGrid;
+    WeightedQuickUnionUF wqfFull;
+
+    int virtualTop;
+    int virtualBottom;
+    int openSites;
 
     public Percolation(int n) {
-        grid = new boolean[n * n];
         if (n <= 0) throw new IllegalArgumentException("enter an int >= 0");
+        this.N = n;
+        this.nSquared = n * n;
+        this.grid = new boolean[nSquared];
+        this.openSites = 0;
+
+        wqfGrid = new WeightedQuickUnionUF(nSquared + 2); // includes virtual top and bottom
+        wqfFull = new WeightedQuickUnionUF(nSquared + 1); // includes virtual top
+
+        this.virtualTop = nSquared;
+        this.virtualBottom = nSquared + 1;
+
+        // merge top row to virtual top of both UF data structures
+        for (int i = 1; i <= N; i++) {
+            wqfGrid.union(i, 0);
+            wqfFull.union(i, 0);
+        }
+        // merge bottom row to virtual bottom
+        for (int i = nSquared; i > (nSquared - n); ++i) {
+            wqfGrid.union(i, nSquared + 1);
+        }
+
     }
 
     public void open(int row, int col) {
+
+        if (!validate(row, col)) {
+            throw new ArrayIndexOutOfBoundsException("Grid coordinates are not valid");
+        }
+        else if (!grid[flatCoords(row, col)]) {
+            int coords = flatCoords(row, col);
+            grid[coords] = true;
+            openSites++;
+        }
+
+        if (isOpen(row - 1, col)) {
+
+        }
+
+        if (isOpen(row, col + 1)) {
+
+        }
+
+        if (isOpen(row + 1, col)) {
+
+        }
+
+        if (isOpen(row, col - 1)) {
+
+        }
     }
 
     public boolean isOpen(int row, int col) {
-        return false;
+        int coords = flatCoords(row, col);
+        return grid[coords];
     }
 
     public boolean ifFull(int row, int col) {
@@ -26,11 +75,22 @@ public class Percolation {
     }
 
     public int numberOfOpenSites() {
-        return 0;
+        return openSites;
     }
 
-    public boolean percolate() {
+    public boolean percolates() {
         return false;
+    }
+
+    private boolean validate(int x, int y) {
+        if (x < 0 && x > N) return false;
+        else if (y < 0 && y > N) return false;
+        return true;
+
+    }
+
+    private int flatCoords(int x, int y) {
+        return ((x - 1) * N) + (y - 1);
     }
 
 }
